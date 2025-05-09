922bf398f143b9ba12ea48746e21de5421697a21
Vijayaravind <161803711+Vijay-Aravind29@users.noreply.github.com>
2025-05-09 13:07:48 +0530

    Update Layout.jsx

--- src/EMI/Components/Layout.jsx
+++ src/EMI/Components/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography } from '@mui/material'; // Added Typography import
+import { Box, Typography } from '@mui/material'; 
 import Header from './Header';
 
 const Layout = ({ children }) => {
